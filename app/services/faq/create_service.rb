4465b3d8cd1ddce219abaa21fa1eca0cfda696b4
André Luiz Leoni <andreluizleoni@gmail.com>
2017-05-21 20:49:10 -0300

    corrigido prblema faq obrigatoria

--- app/services/faq/create_service.rb
+++ app/services/faq/create_service.rb
@@ -9,7 +9,7 @@ module FaqModule
     end
 
     def call
-      return "Hashtag Obrigatória" if @hashtags == nil || @hashtags == ""
+      return "Hashtag Obrigatória" if @hashtags.blank?
 
       begin
         Faq.transaction do
