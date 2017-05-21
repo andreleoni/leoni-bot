5b7728ec3389bad5ae229b33b983dae9511ca0eb
André Luiz Leoni <andreluizleoni@gmail.com>
2017-05-21 20:52:57 -0300

    teste

--- app/services/faq/create_service.rb
+++ app/services/faq/create_service.rb
@@ -9,7 +9,7 @@ module FaqModule
     end
 
     def call
-      return "Hashtag Obrigatória" if @hashtags.blank?
+      # return "Hashtag Obrigatória" if @hashtags.blank?
 
       begin
         Faq.transaction do
